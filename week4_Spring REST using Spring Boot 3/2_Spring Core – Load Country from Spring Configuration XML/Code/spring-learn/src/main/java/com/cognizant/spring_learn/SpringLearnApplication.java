e96c9051670ba33c2c61476602c6f609efda7290
Aparup123 <aparuproychowdhury@gmail.com>
2025-07-10 19:55:03 +0530

    add week4 Load Country from Spring Configuration XML

---
+++ week4_Spring REST using Spring Boot 3/2_Spring Core – Load Country from Spring Configuration XML/Code/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,30 @@
+package com.cognizant.spring_learn;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+	private static final Logger logger= LoggerFactory.getLogger(SpringLearnApplication.class);
+
+	private static void displayCountry(){
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("country.xml");
+		Country country=context.getBean("country", Country.class);
+		logger.debug("Country: {}", country);
+	}
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringLearnApplication.class, args);
+		logger.info("Application running.");
+		displayCountry();
+	}
+
+}
