1f19e06466dcb2310a791dfb77ad6aa0e45a1139
Aparup123 <aparuproychowdhury@gmail.com>
2025-07-11 23:56:13 +0530

    changed from ClassPathXmlResource to ApplicationContext Interface

--- week4_Spring REST using Spring Boot 3/5_REST - Get country based on country code/Code/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ week4_Spring REST using Spring Boot 3/5_REST - Get country based on country code/Code/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,10 +1,10 @@
 package com.cognizant.spring_learn;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.text.SimpleDateFormat;
@@ -15,7 +15,7 @@ public class SpringLearnApplication {
 	private static final Logger logger= LoggerFactory.getLogger(SpringLearnApplication.class);
 
 	private static void displayCountry(){
-		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("country.xml");
+		ApplicationContext context=new ClassPathXmlApplicationContext("country.xml");
 		Country country=context.getBean("country", Country.class);
 		logger.debug("Country: {}", country);
 	}
